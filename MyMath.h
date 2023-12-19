c0a4c4c849f63ceedb290f00f3fd169c882c4b33
Mabe0116 <115063548+Mabe0116@users.noreply.github.com>
2023-12-19 23:55:31 +0900

    板ポリ追加

---
+++ MyMath.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <cmath>
+
+struct Vector4 {
+	float x, y, z, w;
+};
+
+struct Vector3 {
+	float x, y, z;
+};
+
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+struct Transform {
+	Vector3 scale;
+	Vector3 rotate;
+	Vector3 translate;
+};
+
+struct TransformationMatrix {
+	Matrix4x4 WVP;
+};
+
+Matrix4x4 Inverse(const Matrix4x4& m);
+
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip);
+
+Matrix4x4 MakeRotateXMatrix(float radian);
+
+Matrix4x4 MakeIdentity4x4();
+
+Matrix4x4 MakeRotateYMatrix(float radian);
+
+Matrix4x4 MakeRotateZMatrix(float radian);
+
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
