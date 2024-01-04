a48dc7faf5ba18c2538d3f459460a5a47f12daa4
Mabe0116 <115063548+Mabe0116@users.noreply.github.com>
2024-01-04 17:47:20 +0900

    eラー

--- main.cpp
+++ main.cpp
@@ -563,6 +563,100 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	scissorRect.top = 0;
 	scissorRect.bottom = kClientHeight;
 
+	CoInitializeEx(0, COINIT_MULTITHREADED);
+
+	CoUninitialize();
+
+	DirectX::ScratchImage LoadTexture(const std::string & filePath)
+	{
+		//テクスチャファイルを読み込んでプログラムを扱えるようにする
+		DirectX::ScratchImage image{};
+		std::wstring filePathW = ConvertString(filePath);
+		HRESULT hr = DirectX::LoadFrowWICFile(filePathW.c_str(), DirectX::WIC_FLAGS_FORCE_SRGB, nullptr, image);
+		assert(SUCCEEDED(hr));
+
+		//ミニマップの作成
+		DirectX::ScratchImage mipImages{};
+		hr = DirectX::GenerateMipMaps(image.GetImages(), image.GetImageCount(), image.GetMetadata(), DirectX::TEX_FILTER_SRGB, 0, mipImages);
+		assert(SUCCEEDED(hr));
+
+		//ミップマップ付きのデータ返す
+		return mipImages;
+	}
+
+	ID3D12Resource* CreateTextureResource(ID3D12Device* device, const DirectX::TexMetadata& metadata)
+	{
+
+	}
+
+	//metadataを基にResourceの設定
+	D3D12_RESOURCE_DESC resourceDesc{};
+	resourceDesc.Width = UINT(metadata.width);
+	resourceDesc.Height = UINT(metadata.height);
+	resourceDesc.MipLevels = UINT16(metadata.mipLevels);
+	resourceDesc.DepthOrArraySize = UINT16(metadata.arraySize);
+	resourceDesc.Format = metadata.format;
+	resourceDesc.SampleDesc.Count = 1;
+	resourceDesc.Dimension = D3D12_RESOURCE_DIMENSION(metadata.dimension);
+
+	//利用するHeapの設定。非常に特殊な運用。
+	D3D12_HEAP_PROPERTIES heapProperties{};
+	heapProperties.Type = D3D12_HEAP_TYPE_CUSTOM;
+	heapProperties.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_WRITE_BACK;
+	heapProperties.MemoryPoolPreference = D3D12_MEMORY_POOL_L0;
+
+	//Resourceの作成
+	ID3D12Resource* resource = nullptr;
+	HRESULT hr = device->CreateCommittedResource(
+		&heapProperties,
+		D3D12_HEAP_FLAG_NONE,
+		&resourceDesc,
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&resource));
+	assert(SUCCEEDED(hr));
+	return resource;
+
+	void UpLoadTextureData(ID3D12Resouce* texture, const DirectX::ScratchImage& mipImages)
+	{
+		//Meta情報を取得
+		const DirectX::TexMetadata& metadata = mipImages.GetMetadata();
+		//全MipMapについて
+		for (size_t mipLevel = 0; mipLevel < metadata.mipdata.mipLevels; ++mipLevel) {
+			//MipMapLevelを指定して各Imageを取得
+			const DirectX::Image* img = mipImages.GetImage(mipLevel, 0, 0);
+			//Textureに転送
+			HRESULT hr = texture->WriteToSubresource(
+				UINT(mipLevel),
+				nullptr,
+				img->pixels,
+				UINT(img->rowPitch),
+				UINT(img->slicePitch)
+			);
+			assert(SUCCEEDED(hr));
+		}
+
+	}
+
+	//Textureを読み込んで転送する
+	DirectX::ScratchImage mipImages = LoadTexture("resource/uvChecker.png");
+	const DirectX::TexMetadata& metadata = mipImage.GetMetadata();
+	ID3D12Resource* textureResource = CreateTextureResource(device, metadata);
+	UpLoadTextureData(textureResource, mipImages);
+
+	//metaDataを基にSRVの設定
+	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{};
+	srvDesc.Format = metadata.format;
+	srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+	srvDesc.Texture2D.MipLevels = UINT(metadata.mipLevels);
+
+	//SRVを作成するDescriptorHeapの設定場所を決める
+	D3D12_CPU_DESCRIPTOR_HANDLE textureSrvHandleCPU = srvDescriptorHeap->GetCPUDescriptorHandleForHeapStart();
+	D3D12_GPU_DESCRIPTOR_HANDLE textureSrvHandleGPU = srvDescriptorHeap->GetGPUDescriptorHandleForHeapStart();
+	//先頭はImGuiが使っているのでその次を使う
+
+
 
 	//マテリアル用のリソースを作る。今回はcolor１つ分のサイズを用意する
 	ID3D12Resource* materialResource = CreateBufferResource(device, sizeof(Vector4));
